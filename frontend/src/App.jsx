cc3fbbb8d3ea6e788d736a5897e56e89e216af27
erandesamdhan2003 <erandesamadhan2003@gmail.com>
2025-04-05 00:01:48 +0530

    FrontendSetup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import { Home } from './components/Home'
+import { Login } from './auth/Login'
+import { Signup } from './auth/Signup'
 
 const appRouter = createBrowserRouter([
   {
     path: '/',
     element: <Home />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/signup',
+    element: <Signup />
   }
 ])
 export const App = () => {
