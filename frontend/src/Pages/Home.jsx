a7f02e097782b3ef31fd4894e6dd49101bcbfafe
srishti888 <152876543+srishti888@users.noreply.github.com>
2025-04-05 15:40:00 +0530

    home page done

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,6 +1,12 @@
+import { CompareDemo } from "@/components/CompareDemo"
+import { Button } from "@/components/ui/button"
+import { WorldMapDemo } from "@/components/WorldMapDemo"
+
 export const Home = () => {
     return (
         <div>
+            <WorldMapDemo/>
+            <CompareDemo/> 
             
         </div>
     )
